fdb4ce8eea117e8e29c725450bfc2ef37176c58b
sparklyballs <sparklyballs@gmail.com>
2015-10-16 21:23:48 +0100

    fix missing closing ]

--- init/30_set_config.sh
+++ init/30_set_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /config/dbase_and_logs /daapd-pidfolder
 
-[[ ! -f "/config/forked-daapd.conf" ] && cp /defaults/forked-daapd.conf /config/forked-daapd.conf
+[[ ! -f "/config/forked-daapd.conf" ]] && cp /defaults/forked-daapd.conf /config/forked-daapd.conf
 
 cp /config/forked-daapd.conf /etc/forked-daapd.conf
 chown -R abc:abc /config /daapd-pidfolder /app
