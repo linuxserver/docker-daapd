4611c0b12c4644b6652afc818a5efbc16f614204
sparklyballs <sparklyballs@gmail.com>
2015-11-11 17:22:41 +0000

    smylink config file to /etc

--- init/30_set_config.sh
+++ init/30_set_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
+[[ ! -f /config/forked-daapd.conf ]] && cp /defaults/forked-daapd.conf /config/forked-daapd.conf
+[[ ! -L /etc/forked-daapd.conf && -f /etc/forked-daapd.conf ]] && rm /etc/forked-daapd.conf
+[[ ! -L /etc/forked-daapd.conf ]] && ln -s /config/forked-daapd.conf /etc/forked-daapd.conf
+
 mkdir -p /config/dbase_and_logs /daapd-pidfolder
-
-[[ ! -f "/config/forked-daapd.conf" ]] && cp /defaults/forked-daapd.conf /config/forked-daapd.conf
-
-cp /config/forked-daapd.conf /etc/forked-daapd.conf
 chown -R abc:abc /config /daapd-pidfolder /app
